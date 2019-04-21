c0ab95a121b8b76867d17317a8b3293ae080d585
GedorN <gedor.silvaneto@gmail.com>
2019-04-21 01:05:41 -0300

    Melhoria na animação

--- Animation.cpp
+++ Animation.cpp
@@ -1,25 +1,39 @@
 #include <unistd.h>
 #include <iostream>
 #include "Animation.hpp"
-
+#include <map>
+using namespace std;
 Animation::Animation(Matrix *m, list<Vertex*> path) {
     matrix = m->getMatrix();
     rows = m->getRows();
     columns = m->getColumns();
     locX = path.front()->getCoordinates()[0];
     locY = path.front()->getCoordinates()[1];
+    faces[0] = '>';  // EAST
+    faces[1] = '/';  // NORTHEAST
+    faces[2] = '^';  // NORTH
+    faces[3] = '#';  // NORTHWEST
+    faces[4] = '<';  // WEST
+    faces[5] = '%';  // SOUTHWEST
+    faces[6] = 'v';   // SOUTH
+    faces[7] = '\\'; // SOUTHEAST
+    face = 0;
     // print();
     // usleep(1000000);
     std::list<Vertex *>::iterator i;
     // int lenght =  path.size();
     for(i = path.begin(); i != path.end(); i++) {
+        int auxX = locX;
+        int auxY = locY;
         matrix[locX][locY] = '.';
         locX = (*i)->getCoordinates()[0];
         locY = (*i)->getCoordinates()[1];
-        matrix[locX][locY] = NORTH;
+        turn(auxX, auxY);
+        matrix[auxX][auxY] = '.';
+        matrix[locX][locY] = faces[face];
         // path.pop_front();
         print();
-        usleep(1000000);
+        usleep(200000);
         system("clear");
 
     }
@@ -28,80 +42,99 @@ Animation::Animation(Matrix *m, list<Vertex*> path) {
 void Animation::print() {
     int r, c;
     printf("    ");
- for(int i=0;i<columns;i++) 
-   printf("  %d ",i);
- printf("\n");
- printf("    ");
- for(int i=0;i<columns;i++) 
-   printf("+---");
- printf("+\n");
- for( r=0; r < rows ; r++ ) 
- {
- printf("  %d ",r);
-   for( c=0; c < rows; c++ ) {
+    for(int i=0;i<columns;i++) 
+        printf("  %d ",i);
+    printf("\n");
+    printf("    ");
+    for(int i=0;i<columns;i++) 
+        printf("+---");
+    printf("+\n");
+    for( r=0; r < rows ; r++ ) 
+    {
+        printf("  %d ",r);
+        for( c=0; c < rows; c++ ) {
         if (matrix[r][c] == '*') 
-        {
-        printf( "|***" );
-        }
+            {
+                printf( "|***" );
+            }
 
-        if (matrix[r][c] == 'x') 
-        {
-        printf( "| X " );
-        }
+            if (matrix[r][c] == 'x') 
+            {
+                printf( "| X " );
+            }
 
-        if (matrix[r][c] == '.') 
-        {
-        printf( "|   " );
-        }
-        
-        if ( matrix[r][c] == '^' ) {
-            printf("| ^ ");
-        }
-        if ( matrix[r][c] == 'v' ) {
-            printf("| v ");
-        }
-        if ( matrix[r][c] == '>' ) 
-        {
-            printf( "| > " );
-            // if (Rob1.getFacing() == NORTH) 
-            // {
-            //   printf("| ^ ");
-            // }
-            // else if (Rob1.getFacing() == NORTHEAST) 
-            // {
-            //   printf("| / ");
-            // }
-            // else if (Rob1.getFacing() == EAST) 
-            // {
-            //   printf("| > ");
-            // }
-            // else if (Rob1.getFacing() == SOUTHEAST) 
-            // {
-            //   printf("| \\ ");
-            // }
-            // else if (Rob1.getFacing() == SOUTH) 
-            // {
-            //   printf("| v ");
-            // }
-            // else if (Rob1.getFacing() == SOUTHWEST) 
-            // {
-            //   printf("| %% ");
-            // }
-            // else if (Rob1.getFacing() == WEST) 
-            // {
-            //   printf("| < ");
-            // }        
-            // else if (Rob1.getFacing() == NORTHWEST) 
-            // {
-            //   printf("| # ");
-            // }//end of if(Rob1...)else
-        }//end of if(matrix....)
+            if (matrix[r][c] == '.') 
+            {
+                printf( "|   " );
+            }
+            
+            if ( matrix[r][c] == '^' ) {
+                printf("| ^ ");
+            }
+            if ( matrix[r][c] == '/' ) {
+                printf("| / ");
+            }
+            if ( matrix[r][c] == '%' ) {
+                printf("| %% ");
+            }
+            if ( matrix[r][c] == '\\' ) {
+                printf("| \\ ");
+            }
+            if ( matrix[r][c] == '#' ) {
+                printf("| # ");
+            }
+            if ( matrix[r][c] == '<' ) {
+                printf("| < ");
+            }
+            if ( matrix[r][c] == 'v' ) {
+                printf("| v ");
+            }
+            if ( matrix[r][c] == '>' ) 
+            {
+                printf( "| > " );
+            }//end of if(matrix....)
  
-   }//end of for(c)
+        }//end of for(c)
    printf( "|\n" );
    printf("    ");
    for(int i=0;i<columns;i++) 
      printf("+---");
    printf("+\n");
  }//end of for(r)
+}
+
+void Animation::turn(int auxX, int auxY) {
+    char rFace;
+    if(auxY < locY) {
+        if(auxX < locX) {
+            rFace = '\\';
+        } else if (auxX > locX) {
+            rFace = '#';
+        } else {
+            rFace = '>';
+        }
+    } else if (auxY > locY){
+        if(auxX < locX) {
+            rFace = 'v';
+        } else if (auxX > locX) {
+            rFace = '%';
+        } else {
+            rFace ='<';
+        }
+
+    } else {
+        if(auxX < locX) {
+            rFace = 'v';
+        } else {
+            rFace = '^';
+        } 
+    }
+
+    while (rFace != faces[face]) {
+        face = (face + 1 ) % 8;
+        matrix[auxX][auxY] = faces[face];
+        print();
+        usleep(200000);
+        system("clear");
+    }
 }
--- Animation.hpp
+++ Animation.hpp
@@ -7,25 +7,6 @@
 #include "Vertex.hpp"
 #include "Matrix.hpp"
  
-#define ROBOT       26
-#define WALL        27
-#define TARGET	    28
-#define NONE        -1
- 
-#define MAX_ROWS    20
-#define MAX_COLS    20
- 
-#define EAST		0
-#define NORTHEAST	1
-#define NORTH '^'
-#define NORTHWEST       3
-#define WEST		4
-#define SOUTHWEST       5
-#define SOUTH		'v'
-#define SOUTHEAST	7
-
-#define CLOCKWISE    100
-#define COUNTERCLOCK 101
 
 class Animation {
     private:
@@ -35,11 +16,14 @@ class Animation {
         int locY;
         int facing;
         char **matrix;
+        char faces[8];
+        int face;
     public:
         Animation(Matrix* m, list<Vertex*> path);
         void execute();
         int getX();
         int getY();
         void print();
+        void turn(int, int);
 
 };
